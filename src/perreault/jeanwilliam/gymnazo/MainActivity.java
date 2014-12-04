7a2de613c8e630f2111e3031fdb585a707a5e0d0
unknown <1277493@408W22.decinfo.cvm>
2014-12-04 15:26:16 -0500

    Images

--- src/perreault/jeanwilliam/gymnazo/MainActivity.java
+++ src/perreault/jeanwilliam/gymnazo/MainActivity.java
@@ -3,20 +3,30 @@ package perreault.jeanwilliam.gymnazo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button btnCreateRoutine;
+	Button btnLoadRoutine;
+	Button btnPerformance;
+	Button btnSettings;
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		this.btnCreateRoutine = (Button) findViewById(R.id.main_btnCreateRoutine);
+		this.btnLoadRoutine = (Button) findViewById(R.id.main_btnLoadRoutine);
+		this.btnPerformance = (Button) findViewById(R.id.main_btnPerformances);
+		this.btnSettings = (Button) findViewById(R.id.main_btnSettings);
+		
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 
 }
